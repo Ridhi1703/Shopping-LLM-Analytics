96805c3c155510516f4eb4ac66decc116731b7d2
Riddhi Vernekar <vernekarriddhi246@gmail.com>
2025-03-19 23:37:28 +0530

    -

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import {
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword,
     onAuthStateChanged,
-    signOut
+    signOut,
     GoogleAuthProvider,
     signInWithPopup
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
